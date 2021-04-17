b1aacf60ba9280ee0233e6c2d5320d8e2f1d4526
Cory <coryodonoghue1@gmail.com>
2021-04-17 17:00:40 +0100

    Added menu for user

--- menu.py
+++ menu.py
@@ -1,12 +1,21 @@
 # Author: Cory O'Donoghue
 # Graph Theory Project
+import shuntingre
 
-"""
-Main Menu appears when program is running
-"""
-def mainMenu():
-    print("|======================================|")
-    print("|  Author: Cory O'Donoghue- G00376678  |")
-    print("|  Enter main.py --Run to Run program  |")
-    print("|  Enter main.py --Help to View help   |")
-    print("|======================================|")
+
+userAnswer=True
+while userAnswer:
+    print("\n1: Print predefined comparisons\n2: Enter infix expressions and strings\n3: Read from file\n4: Exit")
+    userAnswer = input("Please enter a option: ")
+    # If entry is one print out sample of infix & string comparisions
+    if userAnswer == "1":
+        
+    elif userAnswer == "2":
+        
+    elif userAnswer == "3":
+        
+    elif userAnswer == "4":
+        print("\nGoodbye") 
+        userAnswer = None
+    else:
+        print("\nNot a valid choice, please try again")
