fb702aab2093298ac085ea9040f55383711d7d19
Cory <coryodonoghue1@gmail.com>
2021-04-20 18:56:07 +0100

    added reading from file function & comments

--- menu.py
+++ menu.py
@@ -2,36 +2,78 @@
 # Graph Theory Project
 import shuntingre , thompsonConstruction
 
-def user_input():
-    infixEntry = list(input("\nPlease enter a list or single infix expression: ").split()) 
-    stringEntry = list(input("Please enter a list or single string: ").split()) 
-    print_Input(infixEntry, stringEntry)
+def user_Input():
+    #user enters their regular expression where it is added to a a list 
+    regexInput = input("\nPlease enter your regular expression: ") 
+    #user enters string which the want to match to regex
+    stringEntry = input("Please enter a string you wish to compare: ")
+    #check if user input matches and assign boolean to result
+    result = thompsonConstruction.NFA.match(regexInput, stringEntry)
+
+    #if true print this:
+    if result:
+        print("\nThe regular expression " + regexInput + " matches the string " + stringEntry)
+    #else print this:
+    else:
+        print("\nThe regular expression " + regexInput + " does not match the string " + stringEntry)
+
+
+def file_Input():
+    #user must enter file path
+    myInfixFile  = input("\nPlease enter file path of infixes:")
+    #opening file
+    infix = open(myInfixFile , 'r')
+    #taking contents of the file and making them into a list
+    i_contents = infix.read().splitlines()
+    #print contents of file to screen
+    print(i_contents)
+    #close file
+    infix.close()
+
+    #user must enter file path
+    myStringFile  = input("\nPlease enter file path of Strings:")
+    #opening file
+    strings = open(myStringFile , 'r')
+    #taking contents of the file and making them into a list
+    s_contents = strings.read().splitlines()
+    #printing file contents to screen
+    print(s_contents)
+    #close file
+    infix.close()
+    #printing matching strings
+    print_Input(i_contents, s_contents)
+  
 
 def infix_Examples():
+    #array of infixes & strings
     infixes = ["a.b.a*", "a.(b.b)*.a", "a.b.b+"]
     strings = [ "aba", "abba", "abaa", "abb"]
-    print_Input(infixes, strings)
 
-def print_Input(infixes, strings):
-    print("\nRESULTS\n=======")
+    #loop through infix array
     for i in infixes:
         print()
+        #loop through string array
         for s in strings:
-            print("Infix: %-17s String: %-17s Result: %-5s" % (i, s, thompsonConstruction.NFA.match(i, s)))
+            #print i, s and boolean from match function
+            print("Infix: %-15s String: %-10s Result: %-5s" % (i, s, thompsonConstruction.NFA.match(i, s)))
+
 
 userAnswer=True
 while userAnswer:
-    print("\n1: Print predefined comparisons\n2: Enter infix expressions and strings\n3: Read from file\n4: Exit")
+    print("\n1: Print Regular Expressions examples\n2: Enter infix expressions and strings\n3: Read from file\n4: Exit")
     userAnswer = input("Please enter a option: ")
-    # If entry is one print out sample of infix & string comparisions
+    # If user enters call infix_examples function
     if userAnswer == "1":
         infix_Examples()
+    #if user enters two call user input function
     elif userAnswer == "2":
-        user_input()
+        user_Input()
+    #if user enters 3 , call file_input function
     elif userAnswer == "3":
-        user_input()
+        file_Input()
+    #if user selects 4, close the program
     elif userAnswer == "4":
-        print("\nGoodbye") 
-        userAnswer = None
+        print("\nClosing program") 
+        userAnswer = False
     else:
-        print("\nNot a valid choice, please try again")
+        print("\nThat was not an option, Please try again")
